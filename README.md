d9edbf1b0f9e94c84b2852abd15e4d32acab4d06
Elaphurus <43885619+Lennox-Elaphurus@users.noreply.github.com>
2021-10-09 09:29:20 +0800

    Finish version 1.4.0

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 
 本定投计划计算器，用于在规定投资期数（*投资时间间隔固定*）和投资总额的条件下，按照自定义函数计算每次定投需投入的金额。
 
+*如下面的数学公式不能正确显示，请使用README.pdf*
+
 ## 程序使用
 
 Windows用户双击运行`定投计划计算器.exe`，按照指示输入参数即可。（*首次启动程序耗时较长，之后启动大约需要10秒*） 
@@ -12,34 +14,36 @@ Windows用户双击运行`定投计划计算器.exe`，按照指示输入参数�
 输入：
 - 计划投资的总期数（记为`period`）
 - 计划投入的总金额 (记为`total`)
-- 希望定投金额拟合的函数（已写入程序内，此处选用<img src="https://latex.codecogs.com/png.latex?f(x)=x^2" alt="f(x)=x^2">）
+- 希望定投金额拟合的指数函数的参数`alpha`（$f(x)=x^{\alpha}$）
 
 
 输出：
 - 每期需投入的金额（保存为CSV文件）
-- 期数-当前投入金额 散点图，附带<img src="https://latex.codecogs.com/png.latex?y=x^4, y=x^2, y=\sqrt{x}, y=const, y=x" alt="y=x^4, y=x^2, y=sqrt(x), y=const, y=x" >三条参考线
+- 期数-当前投入金额 散点图，附带$y=x^2, y=\sqrt{x}, y=const, y=x$四条参考线
 
 
 ## 计算步骤
 
-1. 给定一个参考函数(<img src="https://latex.codecogs.com/png.latex?  y=x^{2}" alt="y=x^2">)，取其在`[0,1]`区间上的一段作为标准化函数`f`
-<div align=center><img src="https://latex.codecogs.com/png.latex?f(x)=x^2,x\in\left[0,1\right]"  alt="y=x^2, x in [0,1]"></div>
+1. 给定一个参考函数($f(x)=x^{\alpha}$)，取其在`[0,1]`区间上的一段作为标准化函数`f`
+$$
+f(x)=x^{\alpha},x\in\left[0,1\right]
+$$
 
 2. 根据期数，在`[0,1]`区间均匀地取`period`个点,记为`X`，求得`f`在该点处的值，记为`Y`，取其在`[0,1]`区间上的一段作为标准化函数`f`
-<div  align=center><img src="http://chart.googleapis.com/chart?cht=tx&chl= $$
+$$
 X=\lbrace 0,\frac{1}{period},\frac{2}{period},\frac{2}{period} ,...,1\rbrace \\
 Y=f(X)
-$$" style="border:none;" alt="X={0, 1/period, 2/period, 3/period, ... ,1}"></div>
+$$
 
 3. 将`X`和`Y`缩放到`period`和`total`对应的范围，得到`X'`，`Y'`
 
-<div align=center><img src="http://chart.googleapis.com/chart?cht=tx&chl= $$
+$$
 X^{\prime} = \lbrace period \cdot x \mid x \in X \rbrace \\
 \\
 sum = \sum _{x \in X}  x \\
 Y^{\prime} = \lbrace \frac{total}{sum} \cdot y \mid y \in Y \rbrace \\
-$$" style="border:none;" alt="X'={period*x | x in X}, sum=sigma(X), Y'={total/sum * y | y in Y}"></div>
-4. 此时<img src="https://latex.codecogs.com/png.latex?\sum _{y \in Y^{\prime}} y = total" alt="sigma(Y')=total">，`Y'`即为所求
+$$
+4. 此时$\sum _{y \in Y^{\prime}} y = total$，`Y'`即为所求
 
 
 ## 特性
--- main.py
+++ main.py
@@ -15,11 +15,11 @@ def mysqrt1(x):  #sqrt(x)+b, sympy.integrate(sympy.sqrt(x) + a, (x,i,i+1)
 def quadraticF(x):
     return -x*(x-2)
 
-def sampling(f,period):
+def sampling(alpha,period):
     sampleList=[]
     sum=0
     for i in range(1,period+1):
-        tmpV=f(i/period)
+        tmpV=(i/period)**alpha
         sampleList.append(tmpV)
         sum+=tmpV
     return sampleList,sum
@@ -31,6 +31,7 @@ def saveToCSV(investmentList,period,total):
         csvF=open("投资计划-期数"+str(period)+"-总额"+str(total)+localtime+".csv","w")
     except:
         print("创建文件出错，若已打开表格，请关闭表格后重新运行本程序。")
+        input("按回车键关闭程序...")
         exit(-1)
     csvF.write("期数,当期投资金额\n")
     for i in range(period):
@@ -50,12 +51,8 @@ def mylog(x):
     return math.log10(x+1)
 
 
-def mysquare(x):
-    return x**2
-
-
-def main(f,period,total):
-    standardList,sum=sampling(f,period)
+def main(alpha,period,total):
+    standardList,sum=sampling(alpha,period)
     investmentList = [v * (total/sum) for v in standardList]
     # print(investmentList)
 
@@ -65,12 +62,12 @@ def main(f,period,total):
     plt.xlabel("Period")
     plt.ylabel("Amount")
     localtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    plt.title(localtime)
+    plt.title("Period="+str(period)+" Total="+str(total)+" f(x)=x^"+str(alpha))
 
     xList=np.arange(0,1,0.01)
-    yList=[x**4 * (total/sum) for x in xList]
+    yList=[x**alpha * (total/sum) for x in xList]
     xList=[x * period for x in xList]
-    plt.plot(xList,yList,label="x^4",linestyle="--",color='g')
+    plt.plot(xList,yList,label="x^"+str(alpha),linestyle="--",color='g')
 
     xList=np.arange(0,1,0.01)
     yList=[x**2 * (total/sum) for x in xList]
@@ -80,7 +77,7 @@ def main(f,period,total):
     xList=np.arange(0,1,0.01)
     yList=[math.sqrt(x) * (total/sum) for x in xList]
     xList=[x * period for x in xList]
-    plt.plot(xList,yList,label="sqrt(x)",linestyle="--",color='b')
+    plt.plot(xList,yList,label="x^0.5",linestyle="--",color='b')
 
     xList=np.arange(0,1,0.01)
     yList=[ total/period for x in xList]
@@ -93,7 +90,12 @@ def main(f,period,total):
     plt.plot(xList,yList,label="x",linestyle="--",color='grey')
 
     plt.legend()
-    plt.savefig("Period="+str(period)+"Total="+str(total)+".png")
+    try:
+        plt.savefig("Period="+str(period)+"Total="+str(total)+".png")
+    except:
+        print("创建图片出错，请关闭已打开的图片后重新运行本程序")
+        input("按回车键关闭程序...")
+        exit(-1)
     # plt.show()
     # end draw and save
 
@@ -107,9 +109,9 @@ def main(f,period,total):
 
 period=int(input('请输入定投期数:\n '))
 total=float(input('请输入定投总金额:\n'))
-# print("定投期数:"+str(period),"定投总金额:"+str(total))
+alpha=float(input('请输入函数指数: (例如，sqrt(x)输入0.5,x^2输入2)\n'))
 
-main(mysquare,period,total)
+main(alpha,period,total)
 
 input("按回车键关闭程序...")
 
